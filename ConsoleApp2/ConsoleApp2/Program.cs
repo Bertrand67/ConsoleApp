9c3ce0487bb22ca9a93f71f1235b0f02834e0ed6
Bertrand Schneider <bertrand_schneider@hotmail.com>
2018-01-29 21:49:11 +0100

    Version initiale.

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		private static readonly log4net.ILog log =
+			log4net.LogManager.GetLogger(
+				System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+		private static readonly log4net.ILog confluentKafkaLog =
+			log4net.LogManager.GetLogger(
+				"Confluent.Kafka");
+
+		static void Main(string[] args)
+		{
+			log.Info("Hello logging world!");
+
+			confluentKafkaLog.Error("Confluent.Kafka error!");
+
+			Console.WriteLine("Hit enter");
+			Console.ReadLine();
+		}
+	}
+}
